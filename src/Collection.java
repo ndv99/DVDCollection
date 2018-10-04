ca8fcb36a7470edc0c0f9dca6a1b1e0a88cf0fd9
Nick <noodles524@gmail.com>
2018-10-04 16:55:32 +0100

    Filled out constructor in Collection, creates three DVDs (lsiten in jdoc comment). Added jdoc comment to Collection constructor.

--- src/Collection.java
+++ src/Collection.java
@@ -5,18 +5,31 @@ public class Collection {
      * @param args No params.
      */
 
+    DVD ferrisBueller = new DVD();
+    DVD starWarsIV = new DVD();
+    DVD darkKnight = new DVD();
+
     public static void main(String[] args){
 
-        // new DVD for Ferris Bueller's day off.
-        DVD ferrisBueller = new DVD();
-        ferrisBueller.setTitle("Ferris Bueller's Day Off");
-        ferrisBueller.setDirector("John Hughes");
-        ferrisBueller.setRunTime(103);
-        ferrisBueller.setCost(5);
-
-        ferrisBueller.displayDetails();
+//        test showing that DVD class construction works.
+//        DVD ferrisBueller = new DVD();
+//        ferrisBueller.setTitle("Ferris Bueller's Day Off");
+//        ferrisBueller.setDirector("John Hughes");
+//        ferrisBueller.setRunTime(103);
+//        ferrisBueller.setCost(5);
+//
+//        ferrisBueller.displayDetails();
     }
 
+    /**
+     * Constructor for Collection. Creates three DVD's:
+     * -Star Wars Episode IV
+     * -Ferris Bueller's Day Off
+     * -The Dark Knight
+     */
     public Collection(){
+        ferrisBueller.setAll("Ferris Bueller's Day Off", "John Hughes", 103, 5);
+        starWarsIV.setAll("Star Wars - Episode IV: A new Hope", "George Lucas", 125, 5);
+        darkKnight.setAll("The Dark Knight", "Christpher Nolan", 152, 5);
     }
 }
