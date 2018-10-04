9c865d0abffe62d434c891426999fc76a193c10e
Nick <noodles524@gmail.com>
2018-10-04 15:37:32 +0100

    Added fields "title", "director", "runTime", "cost" to DVD.

--- src/Collection.java
+++ src/Collection.java
@@ -1,2 +1,6 @@
 public class Collection {
+
+    public static void main(){
+
+    }
 }
--- src/DVD.java
+++ src/DVD.java
@@ -1,2 +1,6 @@
 public class DVD {
+    private String title;
+    private String director;
+    private int runTime;
+    float cost;
 }
