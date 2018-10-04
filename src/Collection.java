749d0f1f7e5e2eacd63b32c53995b92349ed726b
Nick <noodles524@gmail.com>
2018-10-04 16:08:49 +0100

    Fixed main method in Collection (forgot to add (String[] args)).

--- src/Collection.java
+++ src/Collection.java
@@ -1,10 +1,14 @@
 public class Collection {
 
-    public static void main(){
+    public static void main(String[] args){
         DVD ferrisBueller = new DVD();
         ferrisBueller.setTitle("Ferris Bueller's Day Off");
         ferrisBueller.setDirector("John Hughes");
         ferrisBueller.setRunTime(103);
         ferrisBueller.setCost(5);
+    }
+
+    public Collection(){
+
     }
 }
